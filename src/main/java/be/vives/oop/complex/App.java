23f5b1b886bd475768bec943a0397bc746367b82
Nico De Witte <nico.dewitte@vives.be>
2018-12-06 08:55:27 +0100

    Add some demo code to main

--- src/main/java/be/vives/oop/complex/App.java
+++ src/main/java/be/vives/oop/complex/App.java
@@ -12,6 +12,26 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      Complex origin = new Complex();
+      System.out.println("Complex origin: " + origin);
+
+      Complex number = new Complex(15, 22);
+      System.out.println("Complex number: " + number);
+
+      Complex base = new Complex(5, 8);
+      System.out.println("Complex base: " + base);
+
+      Complex sum = base.add(number);
+      System.out.println(base + " + " + number + " = " + sum);
+
+      Complex difference = base.subtract(number);
+      System.out.println(base + " - " + number + " = " + difference);
+
+      Complex product = base.multiply(number);
+      System.out.println(base + " . " + number + " = " + product);
+
+      double factor = 10;
+      Complex dividedByFactor = base.divide(factor);
+      System.out.println(base + " / " + factor + " = " + dividedByFactor);
     }
 }
